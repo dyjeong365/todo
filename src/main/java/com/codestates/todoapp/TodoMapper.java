dbd06b81aee06574785eeba8bed84d60de5ca45a
dyjeong365 <dyjeong365@gmail.com>
2023-06-05 15:20:20 +0900

    feat: add TodoMapper

---
+++ src/main/java/com/codestates/todoapp/TodoMapper.java
@@ -0,0 +1,18 @@
+package com.codestates.todoapp;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TodoMapper {
+
+    Todo todoPostToTodo(TodoDto.Post requestBody);
+
+    Todo todoPatchToTodo(TodoDto.Patch requestBody);
+
+    TodoDto.Response todoToTodoResponse(Todo todo);
+
+    List<TodoDto.Response> todosToTodoResponses(List<Todo> todos);
+}

bb54622b11aef90cd4ffc32c5f2d5bfdb9b61555
dyjeong365 <dyjeong365@gmail.com>
2023-06-07 21:38:06 +0900

    fix: add @Column

--- src/main/java/com/codestates/todoapp/Todo.java
+++ src/main/java/com/codestates/todoapp/Todo.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Setter
@@ -17,7 +14,13 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 50, nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private int todoOrder;
+
+    @Column(nullable = false)
     private boolean completed;
 }

557220c7a01f80e6ce824e7767effcbcf7644dc0
dyjeong365 <dyjeong365@gmail.com>
2023-06-07 11:43:14 +0900

    feat: add CorsConfig

---
+++ src/main/java/com/codestates/todoapp/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.codestates.todoapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v1/todos/**")
+                .allowedOrigins("https://todobackend.com/")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
+
